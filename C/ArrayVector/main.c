aea32a038a27d6f0a36b01244a341aace4677ba5
Robin Nabel <robin.nabel@gmail.com>
2016-11-18 10:56:22 +0000

    Initialization code for struct written.

--- C/ArrayVector/main.c
+++ C/ArrayVector/main.c
@@ -3,9 +3,31 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+
+// The data struct
+typedef struct {
+    int size;
+    int currentMaxIndex;
+    int *arr;
+} properties;
+
+properties *initialize() {
+    properties *prop_ptr = (properties *) malloc(sizeof(properties));
+    prop_ptr->size = 8;
+    prop_ptr->currentMaxIndex = 0;
+    prop_ptr->arr = (int *) malloc(prop_ptr->size * sizeof(int)); // Allocate array.
+    return prop_ptr;
+}
 
 int main(void) {
-    printf("Hello World!\n");
+    properties *props = initialize();
+    printf("%d\n", props->size);
+    printf("%d\n", props->currentMaxIndex);
+    for (int i = 0; i < props->size; ++i) {
+        printf("%d\n", props->arr[i]);
+    }
+
     return 0;
 }
 
