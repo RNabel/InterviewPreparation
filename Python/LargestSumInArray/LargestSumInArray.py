67e3b8c95e6d407aa5de5f0ffc74d79d0b6b4d59
robin <rnabel@ucdavis.edu>
2016-11-26 16:15:48 +0000

    Python: Fixed Largest sum in array.

--- Python/LargestSumInArray/LargestSumInArray.py
+++ Python/LargestSumInArray/LargestSumInArray.py
@@ -1,9 +1,12 @@
+import sys
+
+
 def find_largest_sum_contiguous(arr):
     if len(arr):
         sums = [0] * (len(arr) + 1)
         lengths = [0] * (len(arr) + 1)
 
-        for i in range(len(arr) - 1, 0, -1):
+        for i in range(len(arr) - 1, -1, -1):
             current_sum = arr[i]
             current_len = 1
             best_from_next_sum = sums[i + 1]
@@ -17,27 +20,33 @@ def find_largest_sum_contiguous(arr):
             lengths[i] = current_len
 
         # Find best.
-        max_index = sums.index(max(sums))
+        max_index = sums.index(max(sums[:-1]))
         max_cost = sums[max_index]
         max_length = lengths[max_index]
 
         return max_index, max_cost, max_length
     else:
-        return False
+        return 0
 
 
 def find_largest_sum_non_contiguous(arr):
     if len(arr):
-        arr = filter(lambda x: x > 0, arr)
-        return reduce(lambda prev, curr: prev + curr, arr, 0)
+        filtered_arr = filter(lambda x: x > 0, arr)
+        if __name__ == '__main__':
+            if len(filtered_arr):
+                return reduce(lambda prev, curr: prev + curr, filtered_arr)
+            else:  # return largest element
+                return max(arr)
+
     else:
-        return False
+        return 0
 
 
 if __name__ == '__main__':
-    test_arr = [-2, -3, 4, -1, -2, 1, 5, -3]
-    res = find_largest_sum_contiguous(test_arr)
-    print res
-
-    res = find_largest_sum_non_contiguous(test_arr)
-    print res
+    num = int(sys.stdin.readline())
+    for i in range(num):
+        array_length = int(sys.stdin.readline())
+        array = [int(x) for x in sys.stdin.readline().split(" ")]
+        cont_res = find_largest_sum_contiguous(array)[1]
+        non_cont_res = find_largest_sum_non_contiguous(array)
+        print "%d %d" % (cont_res, non_cont_res)
